92b03f3c19cb3a9c020d7a84ee9dd29b9b084410
Adheesh Ramdasi <159422085+codewithaddy13@users.noreply.github.com>
2024-11-10 23:14:14 +0530

    Add files via upload

---
+++ server/mailer.js
@@ -0,0 +1,35 @@
+// mailer.js
+const nodemailer = require('nodemailer');
+
+const sendBookingEmail = async (userEmail, bookingDetails) => {
+  const transporter = nodemailer.createTransport({
+    service: 'gmail', // Ensure you are using the correct service
+    auth: {
+      user: 'sportshubturfs@gmail.com', // Your email address
+      // pass: 'bhcooxfmvslelsif', // Your app password
+      pass: 'pgykyadwjkoxmoec',
+    },
+  });
+
+  const mailOptions = {
+    from: 'adheesh.ramdasi@gmail.com',
+    to: userEmail,
+    subject: 'Booking Confirmation',
+    text: `Dear User,\n\nYour booking has been confirmed with the following details:\n\n` +
+          `Turf Name: ${bookingDetails.turfName}\n` +
+          `Date: ${bookingDetails.date}\n` +
+          `Time: ${bookingDetails.time}\n` +
+          `Duration: ${bookingDetails.duration} hours\n` +
+          `Sport: ${bookingDetails.sport}\n\n` +
+          `Thank you for booking with us!\n\nBest Regards,\nSportsHub Team`,
+  };
+
+  try {
+    await transporter.sendMail(mailOptions);
+    console.log('Email sent successfully');
+  } catch (error) {
+    console.error('Error sending email:', error);
+  }
+};
+
+module.exports = sendBookingEmail;
